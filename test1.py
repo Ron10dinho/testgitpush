9d0e854535f0c6477d310b39a5dc5a2064c7b4aa
Ritwik <ritwik.incredible@gmail.com>
2023-02-14 10:15:42 +0530

    Made a minor change to the error return statement.

--- test1.py
+++ test1.py
@@ -8,5 +8,5 @@ def calculator(operator,num1,num2):
     elif operator == '/':
         return num1 / num2
     else:
-        return "Incorrect input."
+        return "Incorrect input. Try again."
 print(calculator('+',2,5))
